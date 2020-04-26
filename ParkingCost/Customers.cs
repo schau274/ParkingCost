5a25622995d69d55942338fdbe386fc7d06595df
schau274@mail.nmc.edu <schau274@mail.nmc.edu>
2020-04-26 12:51:25 -0400

    Add project files.

---
+++ ParkingCost/Customers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking
+{
+    internal class Customers
+    {
+        //
+        //  Parking cost varibles
+        // 
+
+        const double minimunCharge = 2.0; 
+        const double serviceCharge = 0.5; 
+        const int basicHours = 3;
+        private double _hours;
+
+        
+        public Customers(double hours) 
+        {
+            Hours = hours; 
+        }
+
+        public double Hours 
+        {
+            get { return _hours; }
+            set
+            {
+                if (value < 0 || value > 168)
+                {
+                    Console.WriteLine("Customer can only park for 24 hours");
+                }
+                   
+                else
+                {
+                    _hours = value;
+                }
+                   
+            }
+        }
+
+        public double CaculateCharges()
+        {
+            double totalCharge;
+            
+            if (Hours <= 3)
+                totalCharge = Hours * minimunCharge; 
+
+            else
+                totalCharge = (minimunCharge * basicHours) + ((Hours - basicHours) * (minimunCharge + serviceCharge)); 
+
+            return totalCharge; 
+        }
+    }
+}
---
+++ ParkingCost/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Parking
+{
+    class Program
+    {
+        //  ***************************************************************
+        //  Description: Application can calculate the cost of parking for
+        //               a single customer and all customer 
+        //  Title:       Parking Cost 
+        //  Aurthor:     Andrew Schaub
+        //  Date Modified: 4/26/2020
+        //  ***************************************************************
+        static void Main(string[] args)
+        {
+            //
+            // User input here & varibles 
+            //
+
+            int numOfCustomers; 
+            double hours; 
+            double total = 0.0;
+
+            Console.WriteLine("Enter Number of Customers");
+            numOfCustomers = Convert.ToInt32(Console.ReadLine()); 
+
+            for (int i = 1; i <= numOfCustomers; i++) 
+            {
+                Console.WriteLine("\nEnter the Number of Hours Customer {0} Parked", i);
+                hours = Convert.ToDouble(Console.ReadLine()); 
+                Customers cust = new Customers(hours); 
+                Console.WriteLine("\nCustomer {0} has a total of: {1:C2}", i, cust.CaculateCharges());
+                total += cust.CaculateCharges(); 
+            }
+            //
+            // Total of all
+            //
+
+            Console.WriteLine("The Total Charges For All Customers are {0:C2}", total);
+            Console.ReadLine();
+        }
+    }
+}
